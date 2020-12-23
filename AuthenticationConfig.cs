3b1fc610cf580d2962e6b3b4fad9d70a64b2b95f
Marc van Gorp <Marc.vanGorp@outlook.com>
2020-12-23 17:05:48 +0100

    Initial commit

---
+++ AuthenticationConfig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace MyAadFunction
+{
+    public class Config
+    {
+        /// <summary>
+        /// instance of Azure AD, for example public Azure or a Sovereign cloud (Azure China, Germany, US government, etc ...)
+        /// </summary>
+        public string Instance { get; set; } = "https://login.microsoftonline.com/{0}";
+
+        /// <summary>
+        /// Graph API endpoint, could be public Azure (default) or a Sovereign cloud (US government, etc ...)
+        /// </summary>
+        public string ApiUrl { get; set; } = "https://graph.microsoft.com/";
+
+        /// <summary>
+        /// Reads the configuration from a json file
+        /// </summary>
+        /// <param name="path">Path to the configuration json file</param>
+        /// <returns>AuthenticationConfig read from the json file</returns>
+        public static Config ReadFromJsonFile(string path)
+        {
+            IConfigurationRoot Configuration;
+
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(path);
+
+            Configuration = builder.Build();
+            return Configuration.Get<Config>();
+        }
+    }
+}
---
+++ GetAadUser.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Identity.Client;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Azure.Core;
+using Azure.Identity;
+
+namespace MyAadFunction
+{
+    public static class GetAadUser
+    {
+        [FunctionName("GetAadUser")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            Config config = Config.ReadFromJsonFile("appsettings.json");
+
+            var app = new DefaultAzureCredential();
+
+            string[] scopes = new string[] { $"{config.ApiUrl}.default" };
+
+            AccessToken accessToken;
+
+            try
+            {
+                var tokenContext = new TokenRequestContext(scopes);
+                var cancellationToken = new CancellationToken(default);
+
+                accessToken = await app.GetTokenAsync(tokenContext, cancellationToken);                
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+            {
+                throw;
+            }
+
+            JObject outcome = null;
+            if (accessToken.Token != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+                outcome = await apiCaller.CallWebApiAndProcessResultASync($"{config.ApiUrl}v1.0/users", accessToken.Token);
+            }
+            else
+            {
+                return new UnauthorizedResult();
+            }
+            return new OkObjectResult(outcome);
+        }
+    }
+}
